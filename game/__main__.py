a5299be9af16b717a60803e3a3870e8eb8e2b09a
David Li <david@v1040-wn-rt-b-11-33.campus-dynamic.uwaterloo.ca>
2023-11-24 14:17:29 -0500

    Added more basic game functionality

--- game/__main__.py
+++ game/__main__.py
@@ -7,7 +7,9 @@ from deck import Deck
 newDeck = Deck()
 newDeck.shuffle()
 
-while True:
+play_again = 1
+
+while play_again:
     player1 = Player(100, 0)
 
     bet = int(input("What would you like to bet? "))
@@ -17,29 +19,71 @@ while True:
     dealer_hand = [newDeck.deal(), newDeck.deal()]
     player_hand = [newDeck.deal(), newDeck.deal()]
 
+    newDeck.changeHand(dealer_hand[0][0])
+    newDeck.changeHand(dealer_hand[1][0])
+
     player1.changeHand(player_hand[0][0])
     player1.changeHand(player_hand[1][0])
+
 
     print("The dealer has a " + dealer_hand[0][0] + " of " + dealer_hand[0][1] + " and an unknown card in his hand!")
     print("You received a " + player_hand[0][0] + " of " + player_hand[0][1] + " and a " + player_hand[1][0] + " of " + player_hand[1][1])
     print("You are currently at " + str(player1.hand))
+    print("\n")
 
     while (player1.hand < 21 and input("Hit or stand? (h / s)") == "h"):
         player_hand.append(newDeck.deal())
         player1.changeHand(player_hand[-1][0])
         print("You received a " + player_hand[-1][0] + " of " + player_hand[-1][1] + ". You are currently at " + str(player1.hand))
+        print("\n")
 
-    if (player1.hand > 21):
+    if (player1.hand <= 21): # only do dealer stuff if player hasn't busted
+        print("The dealer has a " + dealer_hand[0][0] + " of " + dealer_hand[0][1] + " and a " + dealer_hand[1][0] + " of " + dealer_hand[1][1] + "!")
+        print("The dealer is at " + str(newDeck.dealer_hand))
+
+        # make dealer keep hitting until they reach 17
+        while (newDeck.dealer_hand < 17):
+            dealer_hand.append(newDeck.deal())
+            newDeck.changeHand(dealer_hand[-1][0])
+            print("The dealer received a " + dealer_hand[-1][0] + " of " + dealer_hand[-1][1] + ". The dealer is currently at " + str(newDeck.dealer_hand))
+            print("\n")
+
+
+    # TODO make a bunch of functions
+
+    # check if player won
+    if (player1.hand > 21): # you busted
         print("You busted!")
         player1.loseBalance(bet)
         print("Your balance is now at " + str(player1.balance))
-    elif (player1.hand == 21):
+    elif (newDeck.dealer_hand > 21): # the dealer busted
+        print("The dealer busted!")
+        player1.addBalance(bet)
+        print("Your balance is now at " + str(player1.balance))
+    elif (player1.hand == 21): # blackjack!
         print("Blackjack!")
         player1.addBalance(bet * 3)
         print("Your balance is now at " + str(player1.balance))
-    else:
-        print("You beat the dealer!")
-        player1.addBalance(bet * 2)
+    elif (player1.hand < newDeck.dealer_hand): # the dealer had the better hand
+        print("The dealer beat you!")
+        player1.loseBalance(bet)
         print("Your balance is now at " + str(player1.balance))
-
-    break
+    elif (player1.hand > newDeck.dealer_hand): # you had the better hand
+        print("You beat the dealer!")
+        player1.addBalance(bet)
+        print("Your balance is now at " + str(player1.balance))
+    elif (player1.hand == newDeck.dealer_hand): # push
+        print("Push!")
+        print("Your balance is still at " + str(player1.balance))
+    
+    play_again = int(input("Do you want to play again? (0 / 1)"))
+    while (play_again != 0 and play_again != 1):
+        play_again = int(input("Input not accepted. Please try again: "))
+    
+    if (play_again):
+        newDeck.resetDeck()
+        player1.hand = 0
+    
+    print("\n")
+    
+print("Thanks for playing!")
--- game/deck.py
+++ game/deck.py
@@ -3,15 +3,28 @@ import random
 class Deck:
     def __init__(
             self,
+            dealer_hand=0,
             card_categories=['Hearts', 'Diamonds', 'Clubs', 'Spades'],
-            cards_list=['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+            cards_list=['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'],
             ):
+        self.dealer_hand = dealer_hand
         self.card_categories = card_categories
         self.cards_list = cards_list
         self.deck = [(card, category) for category in card_categories for card in cards_list]
     
     def shuffle(self):
         random.shuffle(self.deck)
+
+    def changeHand(self, cardNum):
+        if (cardNum == 'Ace'):
+            if (self.dealer_hand <= 10):
+                self.dealer_hand += 11
+            else:
+                self.dealer_hand += 1
+        elif (cardNum == 'Jack' or cardNum == 'Queen' or cardNum == 'King'):
+            self.dealer_hand += 10
+        else:
+            self.dealer_hand += int(cardNum)
 
     def deal(self):
         temp = self.deck[0]
@@ -20,3 +33,5 @@ class Deck:
     
     def resetDeck(self):
         self.deck = [(card, category) for category in self.card_categories for card in self.cards_list]
+        random.shuffle(self.deck)
+        self.dealer_hand = 0

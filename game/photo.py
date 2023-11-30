7433c521dda78c901a93f85f24a3b07af108533a
Andrew Shen <andrew.shen@uwaterloo.ca>
2023-11-29 21:36:34 -0500

    change res

--- game/photo.py
+++ game/photo.py
@@ -3,6 +3,7 @@ from time import sleep
 
 def takePhoto():
     camera = PiCamera()
+    camera.resolution = (400, 300)
     camera.start_preview()
     sleep(0.5)
     camera.capture(f'../pics/scan.jpg')
